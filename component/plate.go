9fa7cfdf80734427e49a58b3cca60206d370bc49
hsldymq <hsldymq@qq.com>
2020-03-18 21:55:26 +0800

    add OffsetValue method

--- component/plate.go
+++ component/plate.go
@@ -24,8 +24,62 @@ type Palace struct {
 	Ninth   int // 9宫
 }
 
-// rotate 按圆盘方式旋转他们的值
-func (p Palace) rotate(by int) Palace {
+// Value 根据宫索引值获得其值
+// 0表示1宫, 1表示2宫, 以此类推
+func (p Palace) Value(palaceIndex int) int {
+	switch palaceIndex {
+	case 0:
+		return p.First
+	case 1:
+		return p.Second
+	case 2:
+		return p.Third
+	case 3:
+		return p.Fourth
+	case 4:
+		return p.Fifth
+	case 5:
+		return p.Sixth
+	case 6:
+		return p.Seventh
+	case 7:
+		return p.Eighth
+	case 8:
+		return p.Ninth
+	default:
+		return -1
+	}
+}
+
+// FindPalaceIndex 根据值找到它所在的宫索引
+// 返回0代表1宫, 返回1代表2宫, 以此类推
+func (p Palace) FindPalaceIndex(value int) int {
+	switch value {
+	case p.First:
+		return 0
+	case p.Second:
+		return 1
+	case p.Third:
+		return 2
+	case p.Fourth:
+		return 3
+	case p.Fifth:
+		return 4
+	case p.Sixth:
+		return 5
+	case p.Seventh:
+		return 6
+	case p.Eighth:
+		return 7
+	case p.Ninth:
+		return 8
+	default:
+		return -1
+	}
+}
+
+// RotateValue 按圆盘方式旋转他们的值
+func (p Palace) RotateValue(by int) Palace {
 	original := [8]int{
 		p.First, p.Eighth, p.Third, p.Fourth,
 		p.Ninth, p.Second, p.Seventh, p.Sixth,
@@ -50,4 +104,63 @@ func (p Palace) rotate(by int) Palace {
 		Sixth:   rotated[7],
 		Fifth:   p.Fifth,
 	}
+}
+
+// OffsetValue 按照宫序进行位移其值
+// 例:
+//		offsetBy == 1时,
+//		1宫的值移到2宫, 2宫的值移到3宫, 以此类推, 9宫的值移到1宫
+//		offsetBy == -1时,
+//		9宫的值移到8宫, 8宫的值移到7宫, 以此类推, 1宫的值移到9宫
+// 当skipFifthPalace为true时, 5宫的值固定,不参与位移, 那么当offsetBy == 1, 4宫的值会移到6宫
+func (p Palace) OffsetValue(offsetBy int, skipFifthPalace bool) Palace {
+	var original [9]int
+	if skipFifthPalace {
+		original = [9]int{
+			p.First, p.Second, p.Third, p.Fourth, p.Sixth, p.Seventh, p.Eighth, p.Ninth, p.Fifth,
+		}
+	} else {
+		original = [9]int{
+			p.First, p.Second, p.Third, p.Fourth, p.Fifth, p.Sixth, p.Seventh, p.Eighth, p.Ninth,
+		}
+	}
+
+	var rotated [9]int
+	rotateNum := 9
+	if skipFifthPalace {
+		rotateNum = 8
+	}
+	if offsetBy < 0 {
+		offsetBy = offsetBy%rotateNum + rotateNum
+	}
+	for i := 0; i < rotateNum; i++ {
+		rotated[(i+offsetBy)%rotateNum] = original[i]
+	}
+
+	if skipFifthPalace {
+		return Palace{
+			First:   rotated[0],
+			Second:  rotated[1],
+			Third:   rotated[2],
+			Fourth:  rotated[3],
+			Fifth:   original[8],
+			Sixth:   rotated[4],
+			Seventh: rotated[5],
+			Eighth:  rotated[6],
+			Ninth:   rotated[7],
+		}
+	} else {
+		return Palace{
+			First:   rotated[0],
+			Second:  rotated[1],
+			Third:   rotated[2],
+			Fourth:  rotated[3],
+			Fifth:   rotated[4],
+			Sixth:   rotated[5],
+			Seventh: rotated[6],
+			Eighth:  rotated[7],
+			Ninth:   rotated[8],
+		}
+	}
+
 }
--- component/plate_test.go
+++ component/plate_test.go
@@ -3,7 +3,7 @@ package component
 import "testing"
 
 func TestPalace(t *testing.T) {
-	t.Run("test rotate method", func(t *testing.T) {
+	t.Run("test RotateValue method", func(t *testing.T) {
 		original := Palace{
 			First:   1,
 			Eighth:  2,
@@ -18,30 +18,73 @@ func TestPalace(t *testing.T) {
 		inputs := []int{3, 9, -2, -28}
 		expect := []Palace{
 			{
-				First: 6, Eighth: 7, Third: 8, Fourth: 1,
-				Ninth: 2, Second: 3, Seventh: 4, Sixth: 5,
+				First: 6, Eighth: 7, Third: 8, Fourth: 1, Ninth: 2, Second: 3, Seventh: 4, Sixth: 5,
 				Fifth: 9,
 			},
 			{
-				First: 8, Eighth: 1, Third: 2, Fourth: 3,
-				Ninth: 4, Second: 5, Seventh: 6, Sixth: 7,
+				First: 8, Eighth: 1, Third: 2, Fourth: 3, Ninth: 4, Second: 5, Seventh: 6, Sixth: 7,
 				Fifth: 9,
 			},
 			{
-				First: 3, Eighth: 4, Third: 5, Fourth: 6,
-				Ninth: 7, Second: 8, Seventh: 1, Sixth: 2,
+				First: 3, Eighth: 4, Third: 5, Fourth: 6, Ninth: 7, Second: 8, Seventh: 1, Sixth: 2,
 				Fifth: 9,
 			},
 			{
-				First: 5, Eighth: 6, Third: 7, Fourth: 8,
-				Ninth: 1, Second: 2, Seventh: 3, Sixth: 4,
+				First: 5, Eighth: 6, Third: 7, Fourth: 8, Ninth: 1, Second: 2, Seventh: 3, Sixth: 4,
 				Fifth: 9,
 			},
 		}
 		for idx, by := range inputs {
-			np := original.rotate(by)
+			np := original.RotateValue(by)
 			if np != expect[idx] {
-				t.Fatalf("rotate palace, expect %+v, got %+v", expect[idx], np)
+				t.Fatalf("RotateValue palace, expect %+v, got %+v", expect[idx], np)
+			}
+		}
+	})
+
+	t.Run("test OffsetValue Method", func(t *testing.T) {
+		original := Palace{
+			First:   1,
+			Second:  2,
+			Third:   3,
+			Fourth:  4,
+			Fifth:   5,
+			Sixth:   6,
+			Seventh: 7,
+			Eighth:  8,
+			Ninth:   9,
+		}
+		nonSkipInputs := []int{1, 5, 9, -1, -5, -10}
+		nonSkipExpect := []Palace{
+			{First: 9, Second: 1, Third: 2, Fourth: 3, Fifth: 4, Sixth: 5, Seventh: 6, Eighth: 7, Ninth: 8},
+			{First: 5, Second: 6, Third: 7, Fourth: 8, Fifth: 9, Sixth: 1, Seventh: 2, Eighth: 3, Ninth: 4},
+			{First: 1, Second: 2, Third: 3, Fourth: 4, Fifth: 5, Sixth: 6, Seventh: 7, Eighth: 8, Ninth: 9},
+			{First: 2, Second: 3, Third: 4, Fourth: 5, Fifth: 6, Sixth: 7, Seventh: 8, Eighth: 9, Ninth: 1},
+			{First: 6, Second: 7, Third: 8, Fourth: 9, Fifth: 1, Sixth: 2, Seventh: 3, Eighth: 4, Ninth: 5},
+			{First: 2, Second: 3, Third: 4, Fourth: 5, Fifth: 6, Sixth: 7, Seventh: 8, Eighth: 9, Ninth: 1},
+		}
+
+		for idx, offset := range nonSkipInputs {
+			actual := original.OffsetValue(offset, false)
+			if actual != nonSkipExpect[idx] {
+				t.Fatalf("offset by %d: expect %+v, got %+v", offset, nonSkipExpect[idx], actual)
+			}
+		}
+
+		skipInputs := []int{1, 4, 10, -1, -5, -13}
+		skipExpect := []Palace{
+			{First: 9, Second: 1, Third: 2, Fourth: 3, Fifth: 5, Sixth: 4, Seventh: 6, Eighth: 7, Ninth: 8},
+			{First: 6, Second: 7, Third: 8, Fourth: 9, Fifth: 5, Sixth: 1, Seventh: 2, Eighth: 3, Ninth: 4},
+			{First: 8, Second: 9, Third: 1, Fourth: 2, Fifth: 5, Sixth: 3, Seventh: 4, Eighth: 6, Ninth: 7},
+			{First: 2, Second: 3, Third: 4, Fourth: 6, Fifth: 5, Sixth: 7, Seventh: 8, Eighth: 9, Ninth: 1},
+			{First: 7, Second: 8, Third: 9, Fourth: 1, Fifth: 5, Sixth: 2, Seventh: 3, Eighth: 4, Ninth: 6},
+			{First: 7, Second: 8, Third: 9, Fourth: 1, Fifth: 5, Sixth: 2, Seventh: 3, Eighth: 4, Ninth: 6},
+		}
+
+		for idx, offset := range skipInputs {
+			actual := original.OffsetValue(offset, true)
+			if actual != skipExpect[idx] {
+				t.Fatalf("skip 5th palace, offset by %d: expect %+v, got %+v", offset, skipExpect[idx], actual)
 			}
 		}
 	})
