5bce1fd3b1f8af6255ec0d465b8f9bf8568f00ed
hsldymq <hsldymq@qq.com>
2020-03-28 20:15:15 +0800

    refactor qiyi

--- component/qiyi.go
+++ component/qiyi.go
@@ -1,81 +1,49 @@
 package component
 
-// 三奇 六仪
+import (
+	"github.com/hsldymq/go-chinese-calendar/sexagenary"
+)
 
-import chinese_calendar "github.com/hsldymq/go-chinese-calendar"
-
-// Qi 三奇类型
-type Qi chinese_calendar.CelestialStem
+// Qi 三奇六仪
+type QiYi sexagenary.CelestialStem
 
 // CelestialStem 转换为对应的天支类型
-func (q Qi) CelestialStem() chinese_calendar.CelestialStem {
-	return chinese_calendar.CelestialStem(q)
+func (q QiYi) CelestialStem() sexagenary.CelestialStem {
+	return sexagenary.CelestialStem(q)
 }
 
 // Value 返回枚举值
-func (q Qi) Value() int {
+func (q QiYi) Value() int {
 	return int(q)
 }
 
-func (q Qi) String() string {
-	qcl := chinese_calendar.CelestialStem(q)
-	if qcl != chinese_calendar.CelestialStemEnum.Yi &&
-		qcl != chinese_calendar.CelestialStemEnum.Bing &&
-		qcl != chinese_calendar.CelestialStemEnum.Ding {
+func (q QiYi) String() string {
+	qcl := sexagenary.CelestialStem(q)
+	if qcl == sexagenary.CelestialStemEnum.Jia {
 		return ""
 	}
 	return qcl.String()
 }
 
-// QiEnum 三奇枚举
-var QiEnum = struct {
-	Yi   chinese_calendar.CelestialStem
-	Bing chinese_calendar.CelestialStem
-	Ding chinese_calendar.CelestialStem
+// QiYiEnum 三奇六仪枚举
+var QiYiEnum = struct {
+	Yi   QiYi
+	Bing QiYi
+	Ding QiYi
+	Wu   QiYi
+	Ji   QiYi
+	Geng QiYi
+	Xin  QiYi
+	Ren  QiYi
+	Gui  QiYi
 }{
-	Yi:   chinese_calendar.CelestialStemEnum.Yi,
-	Bing: chinese_calendar.CelestialStemEnum.Bing,
-	Ding: chinese_calendar.CelestialStemEnum.Ding,
-}
-
-// Yi 六仪类型
-type Yi chinese_calendar.CelestialStem
-
-// CelestialStem 转换为对应的天支类型
-func (y Yi) CelestialStem() chinese_calendar.CelestialStem {
-	return chinese_calendar.CelestialStem(y)
-}
-
-// Value 返回枚举值
-func (y Yi) Value() int {
-	return int(y)
-}
-
-func (y Yi) String() string {
-	qcl := chinese_calendar.CelestialStem(y)
-	if qcl == chinese_calendar.CelestialStemEnum.Wu ||
-		qcl == chinese_calendar.CelestialStemEnum.Ji ||
-		qcl == chinese_calendar.CelestialStemEnum.Geng ||
-		qcl == chinese_calendar.CelestialStemEnum.Jia {
-		return ""
-	}
-
-	return qcl.String()
-}
-
-// YiEnum 三奇枚举
-var YiEnum = struct {
-	Wu   chinese_calendar.CelestialStem
-	Ji   chinese_calendar.CelestialStem
-	Geng chinese_calendar.CelestialStem
-	Xin  chinese_calendar.CelestialStem
-	Ren  chinese_calendar.CelestialStem
-	Gui  chinese_calendar.CelestialStem
-}{
-	Wu:   chinese_calendar.CelestialStemEnum.Wu,
-	Ji:   chinese_calendar.CelestialStemEnum.Ji,
-	Geng: chinese_calendar.CelestialStemEnum.Geng,
-	Xin:  chinese_calendar.CelestialStemEnum.Xin,
-	Ren:  chinese_calendar.CelestialStemEnum.Ren,
-	Gui:  chinese_calendar.CelestialStemEnum.Gui,
+	Yi:   QiYi(sexagenary.CelestialStemEnum.Yi),
+	Bing: QiYi(sexagenary.CelestialStemEnum.Bing),
+	Ding: QiYi(sexagenary.CelestialStemEnum.Ding),
+	Wu:   QiYi(sexagenary.CelestialStemEnum.Wu),
+	Ji:   QiYi(sexagenary.CelestialStemEnum.Ji),
+	Geng: QiYi(sexagenary.CelestialStemEnum.Geng),
+	Xin:  QiYi(sexagenary.CelestialStemEnum.Xin),
+	Ren:  QiYi(sexagenary.CelestialStemEnum.Ren),
+	Gui:  QiYi(sexagenary.CelestialStemEnum.Gui),
 }
