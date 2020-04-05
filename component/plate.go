a84a1743f0fc44e5e88292e25564bd0dd564bebc
hsldymq <hsldymq@qq.com>
2020-04-06 01:16:45 +0800

    refactor plate

--- component/plate.go
+++ component/plate.go
@@ -18,53 +18,35 @@ const (
 
 type PalaceIndex int
 
-func (pi PalaceIndex) RoundDistance(to PalaceIndex, clockwise bool) int {
-	if pi == to || !pi.IsValid() || !to.IsValid() {
+// RoundDistance 计算从当前宫顺时针旋转到指定宫的距离
+func (pi PalaceIndex) RoundDistance(to PalaceIndex) int {
+	if pi == to || !pi.IsValid() || pi == FifthPalace || !to.IsValid() || to == FifthPalace {
 		return 0
 	}
 
 	p := pi
 	distance := 0
 	for ; p != to; distance++ {
-		p = p.RoundNext(clockwise)
+		p = p.RoundNext()
 	}
 	return distance
 }
 
-// RoundNext 返回以旋转方式的下一宫的索引
+// RoundNext 返回以顺时针旋转方式的下一宫的索引
 // clockwise == true以顺时针方式旋转
 // 0 -> 7, 7 -> 2, 2 -> 3, ..., 5 -> 0
-
-// clockwise == false以逆时针方向旋转
-// 0 -> 5, 5 -> 6, 6 -> 1, ..., 7 -> 0
-
 // 4 -> 4, 第五宫始终固定不变
-func (pi PalaceIndex) RoundNext(clockwise bool) PalaceIndex {
-	var pMap map[PalaceIndex]PalaceIndex
-	if clockwise {
-		pMap = map[PalaceIndex]PalaceIndex{
-			FirstPalace:   EighthPalace,
-			SecondPalace:  SeventhPalace,
-			ThirdPalace:   FourthPalace,
-			FourthPalace:  NinthPalace,
-			FifthPalace:   FifthPalace,
-			SixthPalace:   FirstPalace,
-			SeventhPalace: SixthPalace,
-			EighthPalace:  ThirdPalace,
-			NinthPalace:   SecondPalace,
-		}
-	} else {
-		pMap = map[PalaceIndex]PalaceIndex{
-			EighthPalace:  FirstPalace,
-			SeventhPalace: SecondPalace,
-			FourthPalace:  ThirdPalace,
-			NinthPalace:   FourthPalace,
-			FifthPalace:   FifthPalace,
-			FirstPalace:   SixthPalace,
-			SixthPalace:   SeventhPalace,
-			ThirdPalace:   EighthPalace,
-			SecondPalace:  NinthPalace,
-		}
+func (pi PalaceIndex) RoundNext() PalaceIndex {
+	pMap := map[PalaceIndex]PalaceIndex{
+		FirstPalace:   EighthPalace,
+		SecondPalace:  SeventhPalace,
+		ThirdPalace:   FourthPalace,
+		FourthPalace:  NinthPalace,
+		FifthPalace:   FifthPalace,
+		SixthPalace:   FirstPalace,
+		SeventhPalace: SixthPalace,
+		EighthPalace:  ThirdPalace,
+		NinthPalace:   SecondPalace,
 	}
 	return pMap[pi]
 }
@@ -125,22 +107,46 @@ func (pi PalaceIndex) IsValid() bool {
 	return pi >= 0 && pi < 9
 }
 
+type PalaceValue int
+
+func (pv PalaceValue) QiYi() QiYi {
+	return QiYi(pv)
+}
+
+func (pv PalaceValue) Door() Door {
+	return Door(pv)
+}
+
+func (pv PalaceValue) Star() Star {
+	return Star(pv)
+}
+
+func (pv PalaceValue) God() God {
+	return God(pv)
+}
+
 // Palaces 用于保存9宫的值
 // 按照索引, 0代表1宫, 1代表2宫,一次类推
 type Palaces struct {
-	values [9]int
+	values [9]PalaceValue
 }
 
-func NewPalace(values [9]int, startPalaceIndex PalaceIndex, asc bool) (Palaces, error) {
+func NewQiYiPlate(roundPalaceIndex PalaceIndex, escaping Escaping) (Palaces, error) {
 	p := Palaces{}
-	if !startPalaceIndex.IsValid() {
+	if !roundPalaceIndex.IsValid() {
 		return p, fmt.Errorf("invalid start index for creating palace")
 	}
 
-	pIndex := startPalaceIndex
+	values := [9]PalaceValue{
+		PalaceValue(QiYiEnum.Wu), PalaceValue(QiYiEnum.Ji), PalaceValue(QiYiEnum.Geng),
+		PalaceValue(QiYiEnum.Xin), PalaceValue(QiYiEnum.Ren), PalaceValue(QiYiEnum.Gui),
+		PalaceValue(QiYiEnum.Ding), PalaceValue(QiYiEnum.Bing), PalaceValue(QiYiEnum.Yi),
+	}
+
+	pIndex := roundPalaceIndex
 	for i := 0; i < 9; i++ {
 		p.values[pIndex] = values[i]
-		if asc {
+		if escaping == YangEscaping {
 			pIndex = pIndex.Next()
 		} else {
 			pIndex = pIndex.Prev()
@@ -150,9 +156,64 @@ func NewPalace(values [9]int, startPalaceIndex PalaceIndex, asc bool) (Palaces, 
 	return p, nil
 }
 
+// NewOriginalGodPlate 创建原始神盘
+// 阳遁局
+//	|4: 六合|9: 勾陈|2: 朱雀|
+//  |3: 太阴|5  　　|7: 九地|
+//  |8: 腾蛇|1: 值符|6: 九天|
+// 阴遁局
+//	|4: 腾蛇|9: 值符|2: 九天|
+//  |3: 太阴|5  　　|7: 九地|
+//  |8: 六合|1: 白虎|6: 玄武|
+func NewOriginalGodPlate(escaping Escaping) Palaces {
+	var p Palaces
+	if escaping == YangEscaping {
+		p.values = [9]PalaceValue{
+			FourthPalace: PalaceValue(GodEnum.LiuHe), NinthPalace: PalaceValue(GodEnum.GouChen), SecondPalace: PalaceValue(GodEnum.ZhuQue),
+			ThirdPalace: PalaceValue(GodEnum.TaiYin), FifthPalace: -1, SeventhPalace: PalaceValue(GodEnum.JiuDi),
+			EighthPalace: PalaceValue(GodEnum.TengShe), FirstPalace: PalaceValue(GodEnum.ZhiFu), SixthPalace: PalaceValue(GodEnum.JiuTian),
+		}
+	} else {
+		p.values = [9]PalaceValue{
+			FourthPalace: PalaceValue(GodEnum.TengShe), NinthPalace: PalaceValue(GodEnum.ZhiFu), SecondPalace: PalaceValue(GodEnum.JiuTian),
+			ThirdPalace: PalaceValue(GodEnum.TaiYin), FifthPalace: -1, SeventhPalace: PalaceValue(GodEnum.JiuDi),
+			EighthPalace: PalaceValue(GodEnum.LiuHe), FirstPalace: PalaceValue(GodEnum.BaiHu), SixthPalace: PalaceValue(GodEnum.XuanWu),
+		}
+	}
+	return p
+}
+
+// NewOriginalHumanPlate 创建原始人盘
+//	|4: 杜门|9: 景门|2: 死门|
+//  |3: 伤门|5  　　|7: 惊门|
+//  |8: 生门|1: 休门|6: 开门|
+func NewOriginalHumanPlate() Palaces {
+	return Palaces{
+		values: [9]PalaceValue{
+			FourthPalace: PalaceValue(DoorEnum.Du), NinthPalace: PalaceValue(DoorEnum.PJing), SecondPalace: PalaceValue(DoorEnum.Si),
+			ThirdPalace: PalaceValue(DoorEnum.Shang), FifthPalace: -1, SeventhPalace: PalaceValue(DoorEnum.Jing),
+			EighthPalace: PalaceValue(DoorEnum.Sheng), FirstPalace: PalaceValue(DoorEnum.Xiu), SixthPalace: PalaceValue(DoorEnum.Kai),
+		},
+	}
+}
+
+// NewOriginalStarPlate 创建原始九星天盘
+//	|4: 天辅|9: 天英|2: 天芮|
+//  |3: 天冲|5  天禽|7: 天柱|
+//  |8: 天任|1: 天蓬|6: 天心|
+func NewOriginStarPlate() Palaces {
+	return Palaces{
+		values: [9]PalaceValue{
+			FourthPalace: PalaceValue(StarEnum.TianFu), NinthPalace: PalaceValue(StarEnum.TianYing), SecondPalace: PalaceValue(StarEnum.TianRui),
+			ThirdPalace: PalaceValue(StarEnum.TianChong), FifthPalace: PalaceValue(StarEnum.TianQin), SeventhPalace: PalaceValue(StarEnum.TianZhu),
+			EighthPalace: PalaceValue(StarEnum.TianRen), FirstPalace: PalaceValue(StarEnum.TianPeng), SixthPalace: PalaceValue(StarEnum.TianXin),
+		},
+	}
+}
+
 // Value 根据宫索引值获得其值
 // 0表示1宫, 1表示2宫, 以此类推
-func (p Palaces) Value(palaceIndex PalaceIndex) int {
+func (p Palaces) Value(palaceIndex PalaceIndex) PalaceValue {
 	if !palaceIndex.IsValid() {
 		return -1
 	}
@@ -163,7 +224,7 @@ func (p Palaces) Value(palaceIndex PalaceIndex) int {
 // 返回0代表1宫, 返回1代表2宫, 以此类推
 func (p Palaces) FindPalaceIndex(value int) PalaceIndex {
 	for idx, v := range p.values {
-		if value == v {
+		if PalaceValue(value) == v {
 			return PalaceIndex(idx)
 		}
 	}
@@ -187,11 +248,11 @@ func (p Palaces) FindPalaceIndex(value int) PalaceIndex {
 //		3     6
 //		  8 1
 func (p Palaces) RotateValues(by int) Palaces {
-	original := [8]int{
+	original := [8]PalaceValue{
 		p.values[FirstPalace], p.values[EighthPalace], p.values[ThirdPalace], p.values[FourthPalace],
 		p.values[NinthPalace], p.values[SecondPalace], p.values[SeventhPalace], p.values[SixthPalace],
 	}
-	rotated := [8]int{}
+	rotated := [8]PalaceValue{}
 	by = by % 8
 	if by < 0 {
 		by += 8
@@ -201,7 +262,7 @@ func (p Palaces) RotateValues(by int) Palaces {
 		rotated[newIdx] = v
 	}
 	return Palaces{
-		values: [9]int{
+		values: [9]PalaceValue{
 			FourthPalace: rotated[3], NinthPalace: rotated[4], SecondPalace: rotated[5],
 			ThirdPalace: rotated[2], FifthPalace: p.values[FifthPalace], SeventhPalace: rotated[6],
 			EighthPalace: rotated[1], FirstPalace: rotated[0], SixthPalace: rotated[7],
--- component/plate_test.go
+++ component/plate_test.go
@@ -7,7 +7,7 @@ import (
 func TestPalace(t *testing.T) {
 	t.Run("test RotateValues method", func(t *testing.T) {
 		original := Palaces{
-			values: [9]int{
+			values: [9]PalaceValue{
 				FourthPalace: 4, NinthPalace: 9, SecondPalace: 2,
 				ThirdPalace: 3, FifthPalace: 5, SeventhPalace: 7,
 				EighthPalace: 8, FirstPalace: 1, SixthPalace: 6,
@@ -16,28 +16,28 @@ func TestPalace(t *testing.T) {
 		inputs := []int{3, 9, -2, -28}
 		expect := []Palaces{
 			{
-				values: [9]int{
+				values: [9]PalaceValue{
 					FourthPalace: 1, NinthPalace: 8, SecondPalace: 3,
 					ThirdPalace: 6, FifthPalace: 5, SeventhPalace: 4,
 					EighthPalace: 7, FirstPalace: 2, SixthPalace: 9,
 				},
 			},
 			{
-				values: [9]int{
+				values: [9]PalaceValue{
 					FourthPalace: 3, NinthPalace: 4, SecondPalace: 9,
 					ThirdPalace: 8, FifthPalace: 5, SeventhPalace: 2,
 					EighthPalace: 1, FirstPalace: 6, SixthPalace: 7,
 				},
 			},
 			{
-				values: [9]int{
+				values: [9]PalaceValue{
 					FourthPalace: 2, NinthPalace: 7, SecondPalace: 6,
 					ThirdPalace: 9, FifthPalace: 5, SeventhPalace: 1,
 					EighthPalace: 4, FirstPalace: 3, SixthPalace: 8,
 				},
 			},
 			{
-				values: [9]int{
+				values: [9]PalaceValue{
 					FourthPalace: 6, NinthPalace: 1, SecondPalace: 8,
 					ThirdPalace: 7, FifthPalace: 5, SeventhPalace: 3,
 					EighthPalace: 2, FirstPalace: 9, SixthPalace: 4,
@@ -54,18 +54,18 @@ func TestPalace(t *testing.T) {
 
 	t.Run("test OffsetValue Method", func(t *testing.T) {
 		original := Palaces{
-			values: [9]int{1, 2, 3, 4, 5, 6, 7, 8, 9},
+			values: [9]PalaceValue{1, 2, 3, 4, 5, 6, 7, 8, 9},
 		}
 		nonSkipInputs := []int{1, 5, 9, 67, -1, -5, -10, -31}
 		nonSkipExpect := []Palaces{
-			{values: [9]int{9, 1, 2, 3, 4, 5, 6, 7, 8}},
-			{values: [9]int{5, 6, 7, 8, 9, 1, 2, 3, 4}},
-			{values: [9]int{1, 2, 3, 4, 5, 6, 7, 8, 9}},
-			{values: [9]int{6, 7, 8, 9, 1, 2, 3, 4, 5}},
-			{values: [9]int{2, 3, 4, 5, 6, 7, 8, 9, 1}},
-			{values: [9]int{6, 7, 8, 9, 1, 2, 3, 4, 5}},
-			{values: [9]int{2, 3, 4, 5, 6, 7, 8, 9, 1}},
-			{values: [9]int{5, 6, 7, 8, 9, 1, 2, 3, 4}},
+			{values: [9]PalaceValue{9, 1, 2, 3, 4, 5, 6, 7, 8}},
+			{values: [9]PalaceValue{5, 6, 7, 8, 9, 1, 2, 3, 4}},
+			{values: [9]PalaceValue{1, 2, 3, 4, 5, 6, 7, 8, 9}},
+			{values: [9]PalaceValue{6, 7, 8, 9, 1, 2, 3, 4, 5}},
+			{values: [9]PalaceValue{2, 3, 4, 5, 6, 7, 8, 9, 1}},
+			{values: [9]PalaceValue{6, 7, 8, 9, 1, 2, 3, 4, 5}},
+			{values: [9]PalaceValue{2, 3, 4, 5, 6, 7, 8, 9, 1}},
+			{values: [9]PalaceValue{5, 6, 7, 8, 9, 1, 2, 3, 4}},
 		}
 
 		for idx, offset := range nonSkipInputs {
@@ -93,4 +93,40 @@ func TestPalace(t *testing.T) {
 		//	}
 		//}
 	})
+}
+
+func TestNewQiYiPlate(t *testing.T) {
+	p, err := NewQiYiPlate(ThirdPalace, YangEscaping)
+	if err != nil {
+		t.Fatalf("expect a yang escaping terrestrial plate, got error: %s", err)
+	}
+	expect := [9]PalaceValue{
+		PalaceValue(QiYiEnum.Bing), PalaceValue(QiYiEnum.Yi), PalaceValue(QiYiEnum.Wu),
+		PalaceValue(QiYiEnum.Ji), PalaceValue(QiYiEnum.Geng), PalaceValue(QiYiEnum.Xin),
+		PalaceValue(QiYiEnum.Ren), PalaceValue(QiYiEnum.Gui), PalaceValue(QiYiEnum.Ding),
+	}
+
+	for i := 0; i < 9; i++ {
+		pi := PalaceIndex(i)
+		if p.Value(pi) != expect[i] {
+			t.Fatalf("yang escaping, expect value of palace index %d to be %d, got %d", i, expect[i], p.Value(pi))
+		}
+	}
+
+	p, err = NewQiYiPlate(SeventhPalace, YinEscaping)
+	if err != nil {
+		t.Fatalf("expect a yin escaping terrestrial plate, got error: %s", err)
+	}
+	expect = [9]PalaceValue{
+		PalaceValue(QiYiEnum.Ding), PalaceValue(QiYiEnum.Gui), PalaceValue(QiYiEnum.Ren),
+		PalaceValue(QiYiEnum.Xin), PalaceValue(QiYiEnum.Geng), PalaceValue(QiYiEnum.Ji),
+		PalaceValue(QiYiEnum.Wu), PalaceValue(QiYiEnum.Yi), PalaceValue(QiYiEnum.Bing),
+	}
+
+	for i := 0; i < 9; i++ {
+		pi := PalaceIndex(i)
+		if p.Value(pi) != expect[i] {
+			t.Fatalf("yin escaping, expect value of palace index %d to be %d, got %d", i, expect[i], p.Value(pi))
+		}
+	}
 }
